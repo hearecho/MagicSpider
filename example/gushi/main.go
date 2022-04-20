de8829b80306542d5f207de7e5ae2e5bb6532fe5
hearecho <1540302560@qqq.com>
2022-04-20 10:18:11 +0800

    debug

--- example/gushi/main.go
+++ example/gushi/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"github.com/hearecho/MagicSpider"
-	"log"
-	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"runtime/trace"
 )
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+	f, err := os.Create("trace1.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+	defer trace.Stop()
+	//go func() {
+	//	log.Println(http.ListenAndServe("localhost:6060", nil))
+	//}()
 	r := []MagicSpider.Request{{
 			Url:   "https://so.gushiwen.cn/gushi/tangshi.aspx",
 			Parse: NameParse,
